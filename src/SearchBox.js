b810279e02add9cb6cf1a3e7fd502d9af13fcf33
Rebecca Brietzke <rnbrietzke@outlook.com>
2021-12-09 07:37:14 -0600

    add state to app component and console logged the output of the searchbox

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react';
 
-const SearchBox = () => {
-  return <input type="search" placeholder="search robots" />;
+const SearchBox = ({ searchChange }) => {
+  return (
+    <div className="pa2">
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search robots"
+        onChange={searchChange}
+      />
+    </div>
+  );
 };
 
 export default SearchBox;
