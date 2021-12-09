0be2f61db253390e7e123f75c3488ccbdc9d7125
Rebecca Brietzke <rnbrietzke@outlook.com>
2021-12-09 14:07:00 -0600

    Updates

---
+++ precache-manifest.c93cbbdd4ebd41c16bd3ed5285c56113.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4583c162adc138e381d007f42e169b50",
+    "url": "/robofriends-ztm/index.html"
+  },
+  {
+    "revision": "659357dc283c704ab700",
+    "url": "/robofriends-ztm/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "4e6d40c84f26ed5ab843",
+    "url": "/robofriends-ztm/static/css/main.078ebfc5.chunk.css"
+  },
+  {
+    "revision": "659357dc283c704ab700",
+    "url": "/robofriends-ztm/static/js/2.e1b685fc.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/robofriends-ztm/static/js/2.e1b685fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627485cada7abdc42d0d",
+    "url": "/robofriends-ztm/static/js/3.c6b94e83.chunk.js"
+  },
+  {
+    "revision": "4e6d40c84f26ed5ab843",
+    "url": "/robofriends-ztm/static/js/main.b2eae06a.chunk.js"
+  },
+  {
+    "revision": "674d6f3758adeeabc18f",
+    "url": "/robofriends-ztm/static/js/runtime-main.52ef5991.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-ztm/static/media/SEGA.12a3db8b.woff"
+  }
+]);
