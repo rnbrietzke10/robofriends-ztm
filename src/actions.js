7f110a72fb85a2d57e7c92d3bce6afde6dc08fe8
Rebecca Brietzke <rnbrietzke@outlook.com>
2021-12-25 20:09:13 -0600

    add package redux-thunk to handle asynchronous actions

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,19 @@
-import { CHANGE_SEARCH_FIELD } from "./constants";
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED,
+} from "./constants";
+
 export const setSearchField = text => ({
   type: CHANGE_SEARCH_FIELD,
   paylod: text,
 });
+
+export const requestRobots = () => dispatch => {
+  dispatch({ type: REQUEST_ROBOTS_PENDING });
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => dispatch({ type: REQUEST_ROBOTS_SUCCESS, paylod: users }))
+    .catch(err => dispatch({ type: REQUEST_ROBOTS_FAILED, paylod: err }));
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
 import App from "./containers/App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { searchRobots } from "./reducers";
+import { searchRobots, requestRobots } from "./reducers";
 import "tachyons";
 
 const logger = createLogger();
 
-const store = createStore(searchRobots, applyMiddleware(logger));
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
 
 ReactDOM.render(
   <React.StrictMode>
